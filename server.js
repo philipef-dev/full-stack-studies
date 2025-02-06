8b02452dba72e43bf6af1f0ef08249e33906c62b
PhilipeF <philipefdev@gmail.com>
2025-02-06 00:39:14 -0300

    Instalando o configurando o prisma para adicionar e buscar usuarios

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express(); 
-app.use(express.json()); 
+const app = express();
+app.use(express.json());
 
-const users = [];
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
   res.status(200).send(users);
 });
 
-app.post('/', (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.status(201).send(user);
+app.post("/", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+
+  res.status(201).json(req.body);
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
